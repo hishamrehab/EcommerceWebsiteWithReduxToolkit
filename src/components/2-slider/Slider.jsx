5fc45c0014078b483edbee87b5ddffce52ece0be
hishamrehab <hishamrehab309@gmail.com>
2024-02-23 12:06:50 +0200

    sliderSection

--- src/components/2-slider/Slider.jsx
+++ src/components/2-slider/Slider.jsx
@@ -2,38 +2,44 @@ import React from "react";
 import "./Slider.scss";
 // import SLiderItem from "./SliderItem";
 import { sliders } from "../../data";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/scrollbar";
+
+// import required modules
+import { Scrollbar } from "swiper/modules";
+import { Container } from "react-bootstrap";
 const Slider = () => {
   return (
-    <div className="banner">
-      <div className="container">
-        <div className="slider-container has-scrollbar">
-          {sliders.map((slider) => {
-            return (
-              <div className="slider-item">
-                <img
-                  src={slider.image}
-                  alt="banner img"
-                  className="banner-img"
-                />
-                <div className="banner-content">
-                  <p className="banner-subtitle">
-                    {slider.bannerSubtitle}
-                    <h2 className="banner-title">{slider.bannerTitle}</h2>
-                    <p className="banner-text">
-                      Starting at &dollar <b>{slider.price}</b>
-                      .00
-                    </p>
-                    <a href="#" className="banner-btn">
-                      Shop now
-                    </a>
-                  </p>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    </div>
+    <Swiper
+      scrollbar={{
+        hide: true,
+      }}
+      modules={[Scrollbar]}
+      className="mySwiper"
+    >
+      {sliders.map((slider) => {
+        return (
+          <SwiperSlide className="swiper-slide">
+            <img src={slider.image} alt="banner img" className="banner-img" />
+            <div className="banner-content">
+              <p className="banner-subtitle">{slider.bannerSubtitle}</p>
+              <h2 className="banner-title">{slider.bannerTitle}</h2>
+              <p className="banner-text">
+                Starting at &dollar <b>{slider.price}</b>
+                .00
+              </p>
+              <a href="#" className="banner-btn">
+                Shop now
+              </a>
+            </div>{" "}
+            *
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
   );
 };
 
