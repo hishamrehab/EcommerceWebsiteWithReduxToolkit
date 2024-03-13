c2a469cd4bae381a00727a38adf4f6af5f518ebb
hishamrehab <hishamrehab309@gmail.com>
2024-03-13 16:38:07 +0200

    edit mediaQuire of footer section

--- src/components/4-Footer/Footer.jsx
+++ src/components/4-Footer/Footer.jsx
@@ -1,103 +1,107 @@
 import React from "react";
 import "./Footer.css";
-import { Container } from "@mui/material";
+
 import payment from "../../../images/products/payment.png";
 import FooterLinks from "./FooterLinks";
 import PlaceIcon from "@mui/icons-material/Place";
 import CallIcon from "@mui/icons-material/Call";
 import MarkunreadIcon from "@mui/icons-material/Markunread";
+import { Container } from "react-bootstrap";
 
 const Footer = () => {
   return (
-    <div className="footer-section">
-      <div className="brand-directory">
-        <h6 className="brand-title">BRAND DIRECTORY</h6>
-        <div className="fashion">
-          <span className="fristword">FASHION :</span> T-Shirt: Shirts | Shorts
-          & Jeans | Jacket | Dress & Frock | InnerWear | Hosiery
+    <Container>
+      {" "}
+      <div className="footer-section">
+        <div className="brand-directory">
+          <h6 className="brand-title">BRAND DIRECTORY</h6>
+          <div className="fashion">
+            <span className="fristword">FASHION :</span> T-Shirt: Shirts |
+            Shorts & Jeans | Jacket | Dress & Frock | InnerWear | Hosiery
+          </div>
+          <div className="fashion">
+            <span className="fristword">FOOTWEAR : </span> Formal | Boots |
+            Casual | Cowboy Shoes | Safety Shoes | Party Wear Shoes | Branded |
+            Firstcopy | Long Shoes
+          </div>
+          <div className="fashion">
+            <span className="fristword">JEWELLERY : </span> Necklace | Earrings
+            | Couple Rings | Pendants | Crystal | Bangles | Bracelets | Nosepin
+            | Chain | Earrings | Couple Rings{" "}
+          </div>
+          <div className="fashion">
+            <span className="fristword">COSMETICS : </span> Shampoo | Bodywash |
+            Facewash | Makeup Kit | Liner | Lipstick | Prefume | Body Soap |
+            Scrub | Hair Gel | Hair Colors | Hair Dye | Sunscreen | Skin Loson |
+            Liner | Lipstick
+          </div>
         </div>
-        <div className="fashion">
-          <span className="fristword">FOOTWEAR : </span> Formal | Boots | Casual
-          | Cowboy Shoes | Safety Shoes | Party Wear Shoes | Branded | Firstcopy
-          | Long Shoes
-        </div>
-        <div className="fashion">
-          <span className="fristword">JEWELLERY : </span> Necklace | Earrings |
-          Couple Rings | Pendants | Crystal | Bangles | Bracelets | Nosepin |
-          Chain | Earrings | Couple Rings{" "}
-        </div>
-        <div className="fashion">
-          <span className="fristword">COSMETICS : </span> Shampoo | Bodywash |
-          Facewash | Makeup Kit | Liner | Lipstick | Prefume | Body Soap | Scrub
-          | Hair Gel | Hair Colors | Hair Dye | Sunscreen | Skin Loson | Liner |
-          Lipstick
+        <hr
+          style={{
+            color: "#777",
+          }}
+        />
+
+        <div className="Footer-second-section">
+          <div className="top">
+            <div>
+              <FooterLinks
+                itemTitle={"POPULAR CATEGORIES"}
+                item1={"Fashion"}
+                item2={"Electronic"}
+                item3={"Cosmetic"}
+                item4={"Health"}
+                item5={"Watches"}
+              />
+            </div>
+
+            <div>
+              <FooterLinks
+                itemTitle={"PRODUCTS"}
+                item1={"Prices Drop"}
+                item2={"New Products"}
+                item3={"Best Sales"}
+                item4={"Contact Us"}
+                item5={"Sitemap"}
+              />
+            </div>
+
+            <div>
+              <FooterLinks
+                itemTitle={"OUR COMPANY"}
+                item1={"Delivery"}
+                item2={"Legal Notice"}
+                item3={"Terms And Conditions"}
+                item4={"About Us"}
+                item5={"Secure Payment"}
+              />
+            </div>
+
+            <div>
+              <FooterLinks
+                itemTitle={"SERVICES"}
+                item1={"Prices Drop"}
+                item2={"New Products"}
+                item3={"Best Sales"}
+                item4={"Contact Us"}
+                item5={"Sitemap"}
+              />
+            </div>
+          </div>
+
+          <div className="bottom">
+            <div className="right">
+              <img src={payment} className="footer-img" />
+            </div>
+            <div className="left">
+              <span className="copyright">
+                © Copy right 2024 , All Rights Reserved
+              </span>
+            </div>
+          </div>
         </div>
       </div>
-      <hr
-        style={{
-          color: "#777",
-        }}
-      />
-
-      <div className="Footer-second-section">
-        <div className="top">
-          <div>
-            <FooterLinks
-              itemTitle={"POPULAR CATEGORIES"}
-              item1={"Fashion"}
-              item2={"Electronic"}
-              item3={"Cosmetic"}
-              item4={"Health"}
-              item5={"Watches"}
-            />
-          </div>
-
-          <div>
-            <FooterLinks
-              itemTitle={"PRODUCTS"}
-              item1={"Prices Drop"}
-              item2={"New Products"}
-              item3={"Best Sales"}
-              item4={"Contact Us"}
-              item5={"Sitemap"}
-            />
-          </div>
-
-          <div>
-            <FooterLinks
-              itemTitle={"OUR COMPANY"}
-              item1={"Delivery"}
-              item2={"Legal Notice"}
-              item3={"Terms And Conditions"}
-              item4={"About Us"}
-              item5={"Secure Payment"}
-            />
-          </div>
-
-          <div>
-            <FooterLinks
-              itemTitle={"SERVICES"}
-              item1={"Prices Drop"}
-              item2={"New Products"}
-              item3={"Best Sales"}
-              item4={"Contact Us"}
-              item5={"Sitemap"}
-            />
-          </div>
-        </div>
-
-        <div className="bottom">
-          <div className="right">
-            <img src={payment} className="footer-img"  />
-          </div>
-          <div className="left">
-            <span className="copyright">
-              © Copy right 2024 , All Rights Reserved
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
+    </Container>
   );
 };
 
